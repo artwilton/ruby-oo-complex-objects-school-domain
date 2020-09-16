f6109d1c18769db0af87b3c19b12a3a21e15faa8
artwilton <Arthur.M.Wilton@gmail.com>
2020-09-15 20:17:31 -0400

    finished lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+# require 'pry'
+
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name, roster = {} )
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade_number)
+        @roster[grade_number]
+    end
+
+    def sort
+       @roster.map { |k, v| [k, @roster[k].sort] }.to_h
+    end
+
+end
+
+# binding.pry
